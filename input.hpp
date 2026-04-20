8eccd6668dbf1a28404ce4b03a9a5857f877d6d6
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-04-20 14:16:54 -0700

    Input done

---
+++ input.hpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+
+//getInput() function that calls two integer values
+void getInput(int &a, int &b) {
+    //get two integer values
+    cin >> a >> b;
+}
